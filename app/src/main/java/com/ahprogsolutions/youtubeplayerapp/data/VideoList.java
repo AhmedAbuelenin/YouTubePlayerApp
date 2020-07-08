0ca03a1311db3b21bdb042cca379b44b1d51a953
Ahmed <ahprogsolutions@gmail.com>
2020-07-08 22:20:28 +0200

    My 6th commit of branch_01, after adding VideoList.java

---
+++ app/src/main/java/com/ahprogsolutions/youtubeplayerapp/data/VideoList.java
@@ -0,0 +1,23 @@
+package com.ahprogsolutions.youtubeplayerapp.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoList {
+
+    public static List<VideoInfo> getVideos() {
+        List<VideoInfo> list = new ArrayList<>();
+        list.add(new VideoInfo("xEA9vw8t4Ho", "Coding with Mitch",
+                "img_codingwithmitch"));
+        list.add(new VideoInfo("wKFJsrdiGS8", "Coding in Flow",
+                "img_codinginflow"));
+        list.add(new VideoInfo("WX1rSW74lzo", "HaiTHam Motovlog",
+                "img_haitammotovlog"));
+        list.add(new VideoInfo("2vn9PlLemwg", "Joma Tech",
+                "img_jomatech"));
+        list.add(new VideoInfo("4m7msadL5iA", "Microsoft for Startups",
+                "img_microsoftforups"));
+        return list;
+    }
+
+}
