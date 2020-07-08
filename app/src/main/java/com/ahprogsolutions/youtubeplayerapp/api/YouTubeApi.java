81ab72f56968c385863ed950ffc62f6f078750f6
Ahmed <ahprogsolutions@gmail.com>
2020-07-08 13:17:58 +0200

    My first commit, using YouTubePlayerFragment.

---
+++ app/src/main/java/com/ahprogsolutions/youtubeplayerapp/api/YouTubeApi.java
@@ -0,0 +1,10 @@
+package com.ahprogsolutions.youtubeplayerapp.api;
+
+public class YouTubeApi {
+    private static final String API_KEY = "AIzaSyB1lLIeSM1V6M3iyVMolXBUjBGyTsonG8E";
+
+    public static String getApiKey() {
+       return API_KEY;
+    }
+
+}
---
+++ app/src/main/java/com/ahprogsolutions/youtubeplayerapp/ui/HomeActivity.java
@@ -0,0 +1,53 @@
+package com.ahprogsolutions.youtubeplayerapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ahprogsolutions.youtubeplayerapp.R;
+import com.ahprogsolutions.youtubeplayerapp.api.YouTubeApi;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+
+public class HomeActivity extends AppCompatActivity
+        implements YouTubePlayer.OnInitializedListener, View.OnClickListener {
+
+    private YouTubePlayerFragment mYouTubePlayerFragment;
+    private Button playBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        System.out.println("on create: ok");
+        playBtn = findViewById(R.id.play_btn);
+        playBtn.setOnClickListener(this);
+        mYouTubePlayerFragment = (YouTubePlayerFragment) getFragmentManager()
+                .findFragmentById(R.id.youtube_fragment);
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer youTubePlayer, boolean b) {
+        System.out.println("on success: ok");
+        youTubePlayer.loadVideo("W4hTJybfU7s");
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult youTubeInitializationResult) {
+        System.out.println("on failure: something wrong");
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        mYouTubePlayerFragment.initialize(YouTubeApi.getApiKey(), this);
+    }
+}
